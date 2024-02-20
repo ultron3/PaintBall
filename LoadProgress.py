a3a3edfed830369d10b07bfcc71c94b7398802fc
ultron3 <alex.gnavi@gmail.com>
2024-02-20 19:55:46 +0100

    modificata la classe

--- LoadProgress.py
+++ LoadProgress.py
@@ -1,21 +1,20 @@
-from SaveProgres import SaveManager
 import win10toast
-import sys
-import os
 
+class LoadManager:
+    SAVE_FILE_NAME = 'D:\\BounceTale.DAT'
 
-class LoadManager(SaveManager):
     @staticmethod
     def load_game_data():
         try:
             with open(LoadManager.SAVE_FILE_NAME, 'r') as file:
                 data = file.read()
                 print("Dati del gioco caricati:", data)
-         
-                
+                notification = win10toast.ToastNotifier()
+                notification.show_toast("Dati del gioco caricati", data)
         except Exception as e:
             print(f"Errore durante il caricamento: {e}")
-            
+            notification.show_toast(f"Errore durante il caricamento: {e}")
 
 # Esegui il caricamento dei dati del gioco
 LoadManager.load_game_data()
+
